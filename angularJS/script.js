794c081d4e96d59386d9c157de6d330b857fbebf
Angelo Palmieri <palmieri.ang@gmail.com>
2016-12-29 12:54:11 +0000

    controller 14

--- angularJS/script.js
+++ angularJS/script.js
@@ -241,7 +241,45 @@ myApp.controller("myController14", function($scope) {
 
 });
 
+myApp.filter('gender', function(){
+	return function(gender) {
+		switch (gender) {
+			case 1: 
+				return "Male";
+			case 2:
+				return "Female";
+			case 3:
+				return "Not disclosed";
+		}		
+	}
+});
 
+// This way, insted of applying "myModule" to everything (in html tag), I need yo apply myModule2 to this container
+// var app = angular 
+// 	.module("myModule2", [])
+// 	.filter("gender", function() {
+// 		return function(gender) {
+// 			switch (gender) {
+// 				case 1: 
+// 					return "Male";
+// 				case 2:
+// 					return "Female";
+// 				case 3:
+// 					return "Not disclosed";
+// 			}
+// 		}
+// 	})
+// 	.controller("myController14", function($scope) {
+// 		var employees = [
+// 			{ name: "Ben", gender: 1, salary: "55000"},
+// 			{ name: "Sara", gender: 2, salary: "68000"},
+// 			{ name: "Mark", gender: 1, salary: "57000"},
+// 			{ name: "Pam", gender: 2, salary: "53000"},
+// 			{ name: "Todd", gender: 3, salary: "60000"},
+// 		];
+
+// 		$scope.employees = employees;
+// 	});
 
 
 
