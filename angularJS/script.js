c82c11bd4b3eb633bbcac41c5f8f062b12c2ce1b
Angelo Palmieri <palmieri.ang@gmail.com>
2016-12-29 17:04:03 +0000

    lesson 14 and 15

--- angularJS/script.js
+++ angularJS/script.js
@@ -281,7 +281,18 @@ myApp.filter('gender', function(){
 // 		$scope.employees = employees;
 // 	});
 
+myApp.controller("myController15", function($scope) {
+	var employees = [
+		{ name: "Ben", gender: "Male", salary: "55000", city: "London"},
+		{ name: "Sara", gender: "Female", salary: "68000", city: "Chennai"},
+		{ name: "Mark", gender: "Male", salary: "57000", city: "London"},
+		{ name: "Pam", gender: "Female", salary: "53000", city: "Chennai"},
+		{ name: "Todd", gender: "Male", salary: "60000", city: "London"},
+	];
 
+	$scope.employees = employees;
+
+});
 
 
 
