2bd59067a646bf4049c7e5ee1b5615f3e410cce2
Angelo Palmieri <palmieri.ang@gmail.com>
2016-12-29 11:24:22 +0000

    Angular exercises

---
+++ angularJS/script.js
@@ -0,0 +1,251 @@
+/// <reference path="angular.min.js"/>
+
+//1st way
+
+// var myApp = angular.module("myModule", []);
+
+// var myController1 = function($scope) {
+// 	$scope.message = "AngularJS Tutorial";
+// };
+
+// myApp.controller("myController", myController); 
+
+//2nd way
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController1", function($scope) {
+	$scope.message = "AngularJS Tutorial";
+}); 
+
+
+myApp.controller("myController2", function($scope) {
+	var employee = {
+		firstName : "David",
+		lastName : "Hastings",
+		gender : "Male"
+	};
+
+	$scope.employee = employee;
+}); 
+
+//3rd way
+
+// var myApp = angular
+// 				.module("myModule", [])
+// 				.controller("myController2", function($scope) {
+// 	var employee = {
+// 		firstName : "David",
+// 		lastName : "Hastings",
+// 		gender : "Male"
+// 	};
+
+// 	$scope.employee = employee;
+
+// });
+
+myApp.controller("myController3", function($scope) {
+	var country = {
+		name : "USA",
+		capital : "Washington",
+		flag : "usa-flag.svg"
+	};
+
+	$scope.country = country;
+});
+
+myApp.controller("myController4", function($scope) {
+	$scope.message = "Hello Angular";
+});
+
+myApp.controller("myController5", function($scope) {
+	var employee = {
+		firstName : "David",
+		lastName : "Hastings",
+		gender : "Male"
+	};
+
+	$scope.employee = employee;
+
+});
+
+myApp.controller("myController6", function($scope) {
+	var employees = [
+		{firstName:"Ben", lastName:"Hastings", gender:"Male", salary:"55000"},
+		{firstName:"Sara", lastName:"Paul", gender:"Female", salary:"68000"},
+		{firstName:"Mark", lastName:"Holland", gender:"Male", salary:"57000"},
+		{firstName:"Pam", lastName:"Macintosh", gender:"Female", salary:"53000"},
+		{firstName:"Todd", lastName:"Barber", gender:"Male", salary:"60000"}
+	];
+
+	$scope.employees = employees;
+});
+
+myApp.controller("myController7", function($scope) {
+	var countries = [
+		{
+			name: "Uk",
+			cities: [
+				{name: "London"},
+				{name: "Manchester"},
+				{name: "Birmingham"},
+			]
+		},
+		{
+			name: "USA",
+			cities: [
+				{name: "Los Angeles"},
+				{name: "Chicago"},
+				{name: "Huston"},
+			]
+		},
+		{
+			name: "India",
+			cities: [
+				{name: "Hyberabad"},
+				{name: "Chennai"},
+				{name: "Mumbai"},
+			]
+		}
+	];
+
+	$scope.countries = countries;
+});
+
+myApp.controller("myController8", function($scope) {
+	var technologies = [
+		{name: "C#", likes: 0, dislikes: 0},
+		{name: "ASP.NET", likes: 0, dislikes: 0},
+		{name: "SQL", likes: 0, dislikes: 0},
+		{name: "AngularJS", likes: 0, dislikes: 0},		
+	]; 
+
+	$scope.technologies = technologies;
+
+	$scope.incrementLikes = function(technology) {
+		technology.likes++;
+	}
+
+	$scope.incrementDislikes = function(technology) {
+		technology.dislikes++;
+	}
+})
+
+myApp.controller("myController9", function($scope) {
+	var employees = [
+		{ name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: "55000.788"},
+		{ name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: "68000"},
+		{ name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: "57000"},
+		{ name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: "53000"},
+		{ name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: "60000"},
+	];
+
+	$scope.employees = employees;
+
+	$scope.rowLimit = 3;
+});
+
+myApp.controller("myController10", function($scope) {
+	var employees = [
+		{ name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: "55000"},
+		{ name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: "68000"},
+		{ name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: "57000"},
+		{ name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: "53000"},
+		{ name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: "60000"},
+	];
+
+	$scope.employees = employees;
+
+	$scope.sortColumn = "name";
+
+});
+
+myApp.controller("myController11", function($scope) {
+	var employees = [
+		{ name: "Ben", dateOfBirth: new Date("November 23, 1980"), gender: "Male", salary: "55000"},
+		{ name: "Sara", dateOfBirth: new Date("May 05, 1970"), gender: "Female", salary: "68000"},
+		{ name: "Mark", dateOfBirth: new Date("August 15, 1974"), gender: "Male", salary: "57000"},
+		{ name: "Pam", dateOfBirth: new Date("October 27, 1979"), gender: "Female", salary: "53000"},
+		{ name: "Todd", dateOfBirth: new Date("December 30, 1983"), gender: "Male", salary: "60000"},
+	];
+
+	$scope.employees = employees;
+
+	$scope.sortColumn = "name";
+
+	$scope.reverseSort = false;
+
+	$scope.sortData = function (column) {
+		$scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+		$scope.sortColumn = column;
+	}
+
+	$scope.getSortClass = function(column) {
+		if($scope.sortColumn == column) {
+			return $scope.reverseSort ? 'arrow-down' : 'arrow-up';
+		}
+		return '';
+	}
+
+});
+
+myApp.controller("myController12", function($scope) {
+	var employees = [
+		{ name: "Ben", gender: "Male", salary: "55000", city: "London"},
+		{ name: "Sara", gender: "Female", salary: "68000", city: "Chennai"},
+		{ name: "Mark", gender: "Male", salary: "57000", city: "London"},
+		{ name: "Pam", gender: "Female", salary: "53000", city: "Chennai"},
+		{ name: "Todd", gender: "Male", salary: "60000", city: "London"},
+	];
+
+	$scope.employees = employees;
+
+});
+
+myApp.controller("myController13", function($scope) {
+	var employees = [
+		{ name: "Ben", gender: "Male", salary: "55000", city: "London"},
+		{ name: "Sara", gender: "Female", salary: "68000", city: "Chennai"},
+		{ name: "Mark", gender: "Male", salary: "57000", city: "London"},
+		{ name: "Pam", gender: "Female", salary: "53000", city: "Chennai"},
+		{ name: "Todd", gender: "Male", salary: "60000", city: "London"},
+	];
+
+	$scope.employees = employees;
+
+	$scope.search = function(item) {
+		if($scope.searchText == undefined) {
+			return true;
+		} else {
+			if(item.name.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1 || item.city.toLowerCase().indexOf($scope.searchText.toLowerCase()) != -1) {
+				//if is not = -1 we have a match found
+				return true;
+			}
+		}
+		return false;
+		// we don't want that item to be displayed in the search result
+	}
+	
+});
+
+myApp.controller("myController14", function($scope) {
+	var employees = [
+		{ name: "Ben", gender: 1, salary: "55000"},
+		{ name: "Sara", gender: 2, salary: "68000"},
+		{ name: "Mark", gender: 1, salary: "57000"},
+		{ name: "Pam", gender: 2, salary: "53000"},
+		{ name: "Todd", gender: 3, salary: "60000"},
+	];
+
+	$scope.employees = employees;
+
+});
+
+
+
+
+
+
+
+
+
