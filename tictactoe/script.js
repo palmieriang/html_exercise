bcaec4089cbc6cea78c955a1f1ad7eb382283f69
Angelo Palmieri <palmieri.ang@gmail.com>
2017-02-13 17:34:00 +0000

    jQuery

--- tictactoe/script.js
+++ tictactoe/script.js
@@ -9,9 +9,31 @@ $(document).ready(function() {
 	var gameOn = false;
 	//Change player's turn to X and computer's to O
 	$('#turnX').click(function() {
+		console.log(1);
 		turn = "O";
 		computersTurn = "X";
 		$('#turnX').removeClass("btn-primary");
 		$('#turnO').addClass("btn-primary");
+	});
+	//Change player's turn to O and computer's to X
+	$('#turnO').click(function() {
+		console.log(2);
+		turn = "X";
+		computersTurn = "O";
+		$('#turnO').removeClass("btn-primary");
+		$('#turnX').addClass("btn-primary");
+	});
+
+	function playerTurn(turn, id) {
+		var spotTaken = $("#"+id).text();
+		if (spotTaken === "#") {
+			turns[id] = turn;
+			$("#"+id).text(turn);
+		}
+	}
+
+	$('.tic').click(function() {
+		var slot = $(this).attr("id");
+		playerTurn(turn, slot);
 	});
 });
