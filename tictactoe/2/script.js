d1bcfed461c3bc3354773a9536cb60bb6fac244b
Angelo Palmieri <palmieri.ang@gmail.com>
2017-02-14 17:59:02 +0000

    class and script

--- tictactoe/2/script.js
+++ tictactoe/2/script.js
@@ -4,19 +4,49 @@ $(document).ready(function() {
 
 	$('.square').on('click', function() {
 		var squareSelected = $(this);
-		if(squareSelected.hasClass('ex') || squareSelected.hasClass('oh')) {
+		if(squareSelected.hasClass('fa fa-times') || squareSelected.hasClass('fa fa-circle-o')) {
 			console.log('This square has already been selected');
 		} else {
 			// all the rest
 			if (player === 1) {
-				squareSelected.addClass('ex');
-				player = 2;
+				squareSelected.addClass('fa fa-times');
+				// this function will return true or false
+				if(checkIfPlayerWon('fa fa-times')) { 
+					alert('Player ' + player + ' has won!');
+				} else {
+					player = 2;
+				}
 			} else {
-				squareSelected.addClass('oh');
-				player = 1;
+				squareSelected.addClass('fa fa-circle-o');
+				if(checkIfPlayerWon('fa fa-circle-o')) {
+					alert('Player ' + player + ' has won!');
+				} else {
+					player = 1;
+				}
 			}
 		}
 	});
 
+	function checkIfPlayerWon(symbol) {
+		if($('.sq1').hasClass(symbol) && $('.sq2').hasClass(symbol) && $('.sq3').hasClass(symbol)) {
+			return true;
+		} else if($('.sq4').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq6').hasClass(symbol)) {
+			return true;
+		} else if($('.sq7').hasClass(symbol) && $('.sq8').hasClass(symbol) && $('.sq9').hasClass(symbol)) {
+			return true;
+		}else if($('.sq1').hasClass(symbol) && $('.sq4').hasClass(symbol) && $('.sq7').hasClass(symbol)) {
+			return true;
+		}else if($('.sq2').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq8').hasClass(symbol)) {
+			return true;
+		}else if($('.sq3').hasClass(symbol) && $('.sq6').hasClass(symbol) && $('.sq9').hasClass(symbol)) {
+			return true;
+		}else if($('.sq1').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq9').hasClass(symbol)) {
+			return true;
+		}else if($('.sq3').hasClass(symbol) && $('.sq5').hasClass(symbol) && $('.sq7').hasClass(symbol)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 });
