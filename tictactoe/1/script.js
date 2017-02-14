27aba67220e4fe95299c1c71d139818cd32849cf
Angelo Palmieri <palmieri.ang@gmail.com>
2017-02-14 16:52:22 +0000

    new folder

--- tictactoe/1/script.js
+++ tictactoe/1/script.js
@@ -7,6 +7,9 @@ $(document).ready(function() {
 	var computersTurn = "O";
 	//Keeps track if it is the computer's turn
 	var gameOn = false;
+	//Keeps track of computers turn so no loop
+	var count = 0;
+
 	//Change player's turn to X and computer's to O
 	$('#turnX').click(function() {
 		console.log(1);
@@ -14,6 +17,7 @@ $(document).ready(function() {
 		computersTurn = "X";
 		$('#turnX').removeClass("btn-primary");
 		$('#turnO').addClass("btn-primary");
+		reset();
 	});
 	//Change player's turn to O and computer's to X
 	$('#turnO').click(function() {
@@ -22,13 +26,44 @@ $(document).ready(function() {
 		computersTurn = "O";
 		$('#turnO').removeClass("btn-primary");
 		$('#turnX').addClass("btn-primary");
+		reset();
 	});
+
+	function computerTurn(turn, id) {
+		//This variable is used to break the while loop
+		var taken = false;
+		while(taken===false && count!==5) {
+			//Generate computer's random turn
+			var computersMove =(Math.random()*10).toFixed();
+			var move = $('#'+computersMove).text();
+			if(move==='#') {
+				$('#'+computersMove).text(computersTurn);
+				taken = true;
+				turns[computersMove] = computersTurn;
+			} 
+		}
+	}
 
 	function playerTurn(turn, id) {
 		var spotTaken = $("#"+id).text();
 		if (spotTaken === "#") {
+			count++;
 			turns[id] = turn;
 			$("#"+id).text(turn);
+			winCondition(turns, turn);
+			if(gameOn === false) {
+				computerTurn();
+				winCondition(turns, computersTurn);
+			}
+		}
+	}
+
+	function winCondition(turnArray, currentTurn) {
+		if(turnArray[0] === currentTurn && turnArray[1] === currentTurn && turnArray[2] === currentTurn) {
+			gameOn = true;
+			reset();
+		} else {
+			gameOn = false;
 		}
 	}
 
@@ -36,4 +71,11 @@ $(document).ready(function() {
 		var slot = $(this).attr("id");
 		playerTurn(turn, slot);
 	});
+
+	function reset() {
+		turns = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
+		count = 0;
+		$('.tic').text('#');
+		gameOn = false;
+	}
 });
