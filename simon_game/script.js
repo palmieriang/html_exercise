407fb64330a49125e37476e4d3b7b53afa979ea3
Angelo Palmieri <palmieri.ang@gmail.com>
2017-02-22 16:54:43 +0000

    css

--- simon_game/script.js
+++ simon_game/script.js
@@ -1,5 +1,30 @@
 $(document).ready(function() {
-	$('.button').on('click', function() {
-		$(this).addClass('.light');
-	})
+
+	var running = false;
+
+	$('.sw-slot').click(function(){
+		$('#pwr-sw').toggleClass('sw-on');
+		if($('#pwr-sw').hasClass('sw-on')==false){
+			console.log(1);
+			$('.count').text('--');
+			$('.count').addClass('led-off');			
+			$('.push').removeClass('clickable').addClass('unclickable');
+			$('#start').click(function() {
+				startGame();
+			});
+
+		}else{
+			console.log(2);
+			$('.count').removeClass('led-off');
+			running = false;
+		}
+	});
+
+
+	function startGame() {
+		console.log('start');
+	}
+
+
+
 });
