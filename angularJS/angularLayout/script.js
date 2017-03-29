a475b707dc51ab902faf945b365059acd6677649
Angelo Palmieri <palmieri.ang@gmail.com>
2017-03-29 10:01:41 +0100

    Lesson 26

---
+++ angularJS/angularLayout/script.js
@@ -0,0 +1,29 @@
+var app = angular.module("Demo", ["ngRoute"])
+				 .config(function($routeProvider) {
+				 	$routeProvider
+				 		.when("/home", {
+				 			templateUrl: "templates/home.html",
+				 			controller: "homeController"
+				 		})
+				 		.when("/courses", {
+				 			templateUrl: "templates/courses.html",
+				 			controller: "coursesController"
+				 		})
+				 		.when("/students", {
+				 			templateUrl: "templates/students.html",
+				 			controller: "studentsController"
+				 		})
+				 })
+				 .controller("homeController", function($scope) {
+				 	$scope.message = "Home Page";
+				 })
+				 .controller("coursesController", function($scope) {
+				 	$scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
+				 })
+				 .controller("studentsController", function($scope, $http) {
+				 	$http.get("StudentService.asmx/GetAllStudents")
+				 		 .then(function(response) {
+							$scope.students = response.data;
+				 		 })
+				 })
+
