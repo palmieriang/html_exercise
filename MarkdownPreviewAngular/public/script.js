9dd6032a12aaee9ff37ceeb656679b4ef0878476
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-05 18:08:34 +0100

    sorted

--- MarkdownPreviewAngular/public/script.js
+++ MarkdownPreviewAngular/public/script.js
@@ -1,43 +1,16 @@
 var myApp = angular.module('myModule', ['ngSanitize', 'markdown']);
 
-	myApp.controller("myController0", function($scope) {
+	myApp.controller("myController1", function($scope) {
 		$scope.snippet = "# test";
-	})
-	.filter('markdown', function() {
-	
-		return function(text) {
-
-			if (typeof text == "undefined") {
-				return "";
-			}
-			return markdown.toHTML(String(text));
-		}
-	
 	});
 
-	myApp.controller("myController1", ['$scope', function($scope) {
+	myApp.controller("myController2", ['$scope', function($scope) {
 		$scope.snippet = "## test";
-	}])
-	.filter('markdown', function() {
-	
-		return function(text) {
+	}]);
 
-			if (typeof text == "undefined") {
-				return "";
-			}
-			return markdown.toHTML(String(text));
-		}
-	
-	});
-
-	myApp.controller('myController2', ['$scope', '$sce', function($scope, $sce) {
-		$scope.snippet =
-			'### test';
+	myApp.controller('myController3', ['$scope', '$sce', function($scope, $sce) {
+		$scope.snippet = '### test';
 		$scope.deliberatelyTrustDangerousSnippet = function() {
 			return $sce.trustAsHtml($scope.snippet);
 		};
 	}]);
-
-	myApp.controller('myController3', function($scope) {
-		$scope.snippet = '#### test';
-	});
