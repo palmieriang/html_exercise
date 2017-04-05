196dee30a89204f49ee84c139910562ca5f5d8c1
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-05 17:54:17 +0100

    sorted

--- MarkdownPreviewAngular/public/script.js
+++ MarkdownPreviewAngular/public/script.js
@@ -1,24 +1,20 @@
-
-// var myApp = angular.module('myModule', ['ngSanitize']);
-
-// 	myApp.controller("myController", function($scope) {
-// 		$scope.message = "test";
-// 	})
-// 	.filter('markdown', function() {
-	
-// 		return function(text) {
-
-// 			if (typeof text == "undefined") {
-// 				return "";
-// 			}
-// 			// return markdown.toHTML(String(text));
-// 			return text.toUpperCase();
-// 		}
-	
-// });
-
-
 var myApp = angular.module('myModule', ['ngSanitize', 'markdown']);
+
+	myApp.controller("myController0", function($scope) {
+		$scope.snippet = "test";
+	})
+	.filter('markdown', function() {
+	
+		return function(text) {
+
+			if (typeof text == "undefined") {
+				return "";
+			}
+			return markdown.toHTML(String(text));
+			// return text.toUpperCase();
+		}
+	
+	});
 
 	myApp.controller("myController1", ['$scope', function($scope) {
 		$scope.snippet = "# test";
@@ -47,16 +43,3 @@ var myApp = angular.module('myModule', ['ngSanitize', 'markdown']);
 	myApp.controller('myController3', function($scope) {
 		$scope.snippet = '### test';
 	});
-
-// angular.module('myModule', ['ngSanitize'])
-// 	.controller('myController', ['$scope', '$sce', function($scope, $sce) {
-// 		$scope.snippet =
-// 			'<p style="color:blue">an html\n' +
-// 			'<em onmouseover="this.textContent=\'PWN3D!\'">click here</em>\n' +
-// 			'snippet</p>';
-// 		$scope.deliberatelyTrustDangerousSnippet = function() {
-// 			return $sce.trustAsHtml($scope.snippet);
-// 		};
-// 	}]);
-
-
