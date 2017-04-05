8fe4ef8f81ff9d955405ae60ef863bd35e829811
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-05 18:11:58 +0100

    sorted

--- MarkdownPreviewAngular/public/script.js
+++ MarkdownPreviewAngular/public/script.js
@@ -7,10 +7,3 @@ var myApp = angular.module('myModule', ['ngSanitize', 'markdown']);
 	myApp.controller("myController2", ['$scope', function($scope) {
 		$scope.snippet = "## test";
 	}]);
-
-	myApp.controller('myController3', ['$scope', '$sce', function($scope, $sce) {
-		$scope.snippet = '### test';
-		$scope.deliberatelyTrustDangerousSnippet = function() {
-			return $sce.trustAsHtml($scope.snippet);
-		};
-	}]);
