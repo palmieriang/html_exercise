8045e81fec530ffb6063cdce7e5fb14538bab31e
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-05 16:44:07 +0100

    different controller

--- MarkdownPreviewAngular/public/script.js
+++ MarkdownPreviewAngular/public/script.js
@@ -18,8 +18,25 @@
 // });
 
 
-angular.module('myModule', ['ngSanitize'])
-	.controller('myController', ['$scope', '$sce', function($scope, $sce) {
+var myApp = angular.module('markdown', ['ngSanitize', 'markdown']);
+
+	myApp.controller("myController1", ['$scope', function($scope) {
+		$scope.snippet = "#test";
+	}])
+	.filter('markdown', function() {
+	
+		return function(text) {
+
+			if (typeof text == "undefined") {
+				return "";
+			}
+			// return markdown.toHTML(String(text));
+			return text.toUpperCase();
+		}
+	
+	});
+
+	myApp.controller('myController2', ['$scope', '$sce', function($scope, $sce) {
 		$scope.snippet =
 			'<p style="color:blue">an html\n' +
 			'<em onmouseover="this.textContent=\'PWN3D!\'">click here</em>\n' +
@@ -29,21 +46,19 @@ angular.module('myModule', ['ngSanitize'])
 		};
 	}]);
 
+	myApp.controller('myController3', function($scope) {
+		$scope.snippet = '# Heading 1\n- [Link](http://example.com)\n- [Custom Link 1](herp://is.this.working?)\n- [Custom Link 2](derp://is.this.working?)';
+	});
 
-// var myApp = angular.module('myModule', ['ngSanitize']);
+// angular.module('myModule', ['ngSanitize'])
+// 	.controller('myController', ['$scope', '$sce', function($scope, $sce) {
+// 		$scope.snippet =
+// 			'<p style="color:blue">an html\n' +
+// 			'<em onmouseover="this.textContent=\'PWN3D!\'">click here</em>\n' +
+// 			'snippet</p>';
+// 		$scope.deliberatelyTrustDangerousSnippet = function() {
+// 			return $sce.trustAsHtml($scope.snippet);
+// 		};
+// 	}]);
 
-// 	myApp.controller("myController", ['$scope', '$sce', function($scope, $sce) {
-// 		$scope.snippet = "test";
-// 	}])
-// 	.filter('markdown', function() {
-	
-// 		return function(text) {
 
-// 			if (typeof text == "undefined") {
-// 				return "";
-// 			}
-// 			// return markdown.toHTML(String(text));
-// 			return text.toUpperCase();
-// 		}
-	
-// });
