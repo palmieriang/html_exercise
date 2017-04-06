dc2d35339586c323fd992488eafd7d3b5e7e0bfb
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-06 09:58:04 +0100

    folder changed
    moved to AngularJS folder

--- MarkdownPreviewAngular/public/script.js
+++
@@ -1,9 +0,0 @@
-var myApp = angular.module('myModule', ['ngSanitize', 'markdown']);
-
-	myApp.controller("myController1", function($scope) {
-		$scope.snippet = "# test";
-	});
-
-	myApp.controller("myController2", ['$scope', function($scope) {
-		$scope.snippet = "## test";
-	}]);
