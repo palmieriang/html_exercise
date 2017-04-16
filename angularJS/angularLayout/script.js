c44ac589d5afabe202f4b712f6b6ba2a4ddc20fc
Angelo Palmieri <palmieri.ang@gmail.com>
2017-04-16 23:28:04 +0100

    Lesson

--- angularJS/angularLayout/script.js
+++ angularJS/angularLayout/script.js
@@ -17,46 +17,13 @@ var app = angular.module("Demo", ["ngRoute"])
 				 })
 				 .controller("homeController", function($scope) {
 				 	$scope.message = "Home Page";
-				 	console.log('OOKK');
 				 })
 				 .controller("coursesController", function($scope) {
 				 	$scope.courses = ["C#", "VB.NET", "SQL Server", "ASP.NET"];
-				 	console.log('OOKK');
 				 })
 				 .controller("studentsController", function($scope, $http) {
 				 	$http.get("http://localhost/exercises/lesson25.php")
 				 		 .then(function(response) {
 							$scope.students = response.data;
-				 	console.log('OOKK');
 				 		 })
 				 })
-
-// var app = angular
-//             .module("Demo", ["ngRoute"])
-//             .config(function ($routeProvider) {
-//                 $routeProvider
-//                     .when("/home", {
-//                         templateUrl: "templates/home.html",
-//                         controller: "homeController"
-//                     })
-//                     .when("/courses", {
-//                         templateUrl: "templates/courses.html",
-//                         controller: "coursesController"
-//                     })
-//                     .when("/students", {
-//                         templateUrl: "templates/students.html",
-//                         controller: "studentsController"
-//                     })
-//             })
-//             .controller("homeController", function ($scope) {
-//                 $scope.message = "Home Page";
-//             })
-//             .controller("coursesController", function ($scope) {
-//                 $scope.courses = ["C#", "VB.NET", "ASP.NET", "SQL Server"];
-//             })
-//              .controller("studentsController", function ($scope, $http) {
-//                  $http.get("http://localhost/exercises/3connectAngularJS/test.php")
-//                                         .then(function (response) {
-//                                             $scope.students = response.data;
-//                                         })
-//              })
