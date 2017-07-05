aa5257b3dee03bae11e90a5a10fd7ccfcb8d5586
Angelo Palmieri <palmieri.ang@gmail.com>
2017-07-05 21:10:21 +0100

    new repo created

--- calculator_js_IPhone/time.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-	var minutes = ('0' + new Date().getMinutes()).slice(-2);
-	var hours = ('0' + new Date().getHours()).slice(-2);
-	var time = hours + ':' + minutes;
-
-	$(".time").html(time);
-});
